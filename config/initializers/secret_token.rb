d18fccb7aa946ddc9f5d8d01c6a5b9c546a9d251
Fiona Tay <me@fionatay.com>
2012-12-23 12:13:12 -0800

    starter files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuggestotronBackbone::Application.config.secret_token = '7fa67e82deafff118d19ef62846d4141b55e61692df0d66335a62a878a8eb32265a89094ed36af9331ab64a5ea93fc448cd2ad0332f15caf3ca2ea3a368aa856'
