3f693e5ee4c213201c12cb77d433b88e17a25a49
Fiona Tay <me@fionatay.com>
2013-01-15 22:14:12 -0800

    add api docs

--- Gemfile
+++ Gemfile
@@ -4,9 +4,11 @@ gem 'rails', '3.2.11'
 gem 'pg'
 
 # Assets
+gem 'backbone-on-rails'
 gem 'jquery-rails'
 gem 'uglifier'
-gem 'backbone-on-rails'
+
+gem 'rspec_api_documentation', github: 'GreenplumChorus/rspec_api_documentation'
 
 group :test, :development do
   gem 'rspec-rails', '~> 2.0'
---
+++ spec/acceptance/topics_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+resource 'Topic' do
+  get '/topics' do
+    example_request 'Get a list of all topics' do
+      status.should == 200
+    end
+  end
+
+  get '/topics/:id' do
+    let(:topic) { Topic.first }
+    let(:id) { topic.id }
+
+    example_request 'Get a topic' do
+      status.should == 200
+    end
+  end
+
+  post '/topics' do
+    let(:title) { 'A New Topic 1' }
+    let(:description) { 'Description for A New Topic :)' }
+
+    parameter :title, 'Topic title'
+    parameter :description, 'Topic description'
+    required_parameters :title
+
+    example_request 'Create a new topic' do
+      status.should == 201
+    end
+  end
+end
