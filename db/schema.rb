59bdda5ba4d06e0ca45b49f2d2f5fcd9100ec95d
Fiona Tay <me@fionatay.com>
2013-02-27 00:34:02 -0800

    upgrade to edge rails

--- db/schema.rb
+++ db/schema.rb
@@ -9,21 +9,24 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121223200516) do
+ActiveRecord::Schema.define(version: 20121223200516) do
 
-  create_table "topics", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "topics", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "votes", :force => true do |t|
+  create_table "votes", force: true do |t|
     t.integer  "topic_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
