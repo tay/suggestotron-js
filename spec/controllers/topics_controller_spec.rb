97dbb24d3df0920ed1154a23fa7e6b0be2e53456
Fiona Tay <me@fionatay.com>
2013-02-27 00:33:47 -0800

    add tests for TopicsController

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -7,4 +7,17 @@ describe TopicsController do
       response.should be_success
     end
   end
+
+  describe '#create' do
+    it 'has the right success code' do
+      post :create, topic: { title: 'A Topic' }
+      response.status.should == 201
+    end
+
+    it 'creates a new item' do
+      expect {
+        post :create, topic: { title: 'A Topic' }
+      }.to change(Topic, :count).by(1)
+    end
+  end
 end
